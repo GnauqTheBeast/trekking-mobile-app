c3e9d827d016757484a06a63a7a997e8d69cdd1c
duckii <duckiiduckie@gmail.com>
2025-05-13 22:21:38 +0700

    update decode pubsub go

--- notification-service/internal/types/pubsub.go
+++ notification-service/internal/types/pubsub.go
@@ -1,11 +1,20 @@
 package types
 
-import "github.com/vmihailenco/msgpack/v5"
+import (
+	"encoding/json"
+	"github.com/vmihailenco/msgpack/v5"
+	"time"
+)
 
 type PaymentBookingResult struct {
-	UserId  string `json:"userId"`
-	Result  string `json:"result"`
-	Message string `json:"message"`
+	ID        string    `json:"Id"`
+	BookingID string    `json:"BookingId"`
+	Total     int64     `json:"Total"`
+	Method    string    `json:"Method"`
+	Type      string    `json:"Type"`
+	Status    string    `json:"Status"`
+	CreatedAt time.Time `json:"CreatedAt"`
+	UpdatedAt time.Time `json:"UpdatedAt"`
 }
 
 type Ping struct {
@@ -14,7 +23,7 @@ type Ping struct {
 
 func UnmarshalPaymentBookingResult(msg []byte) (interface{}, error) {
 	paymentBookingResult := new(PaymentBookingResult)
-	err := msgpack.Unmarshal(msg, paymentBookingResult)
+	err := json.Unmarshal(msg, paymentBookingResult)
 	return paymentBookingResult, err
 }
 
